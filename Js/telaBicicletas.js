f6a56b214fee473ebcd8474e4b90256041650d96
LeonardoBarros404 <leonardo.barros0337@gmail.com>
2024-11-17 14:01:11 -0300

    Update telaBicicletas.js

--- Js/telaBicicletas.js
+++ Js/telaBicicletas.js
@@ -1,1 +1,9 @@
-
+document.querySelectorAll('.rent-button').forEach(button => {
+        button.addEventListener('click', function () {
+            const bikeNome = this.getAttribute('data-nome');
+            const bikeImg = this.getAttribute('data-img');
+    
+            // Salva a bicicleta escolhida no Local Storage
+            localStorage.setItem('bicicletaEscolhida', JSON.stringify({ nome: bikeNome, img: bikeImg }));
+        });
+    });
